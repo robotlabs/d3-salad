c8ac4aee6bb8cb4fff9cd3e419528592fd265cf4
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2018-11-15 17:48:41 +0000

    improved auto-responsive svg in chart-factory

--- src/charts/chart-factory.js
+++ src/charts/chart-factory.js
@@ -1,6 +1,10 @@
 import * as d3 from 'd3';
 import chartIndex from './chart-index';
-// import gauge from './chart/gauge';
+
+const viewBox = {
+  w: 200,
+  h: 200
+}
 const chartFactory = (domEl, chartType, params = {}) => {
   const d3El = d3.select(domEl);
   const w = params.w || domEl.clientWidth;
@@ -12,10 +16,10 @@ const chartFactory = (domEl, chartType, params = {}) => {
 const createG = (d3El, w, h) => {
   return d3El.append('svg')
     .attr('preserveAspectRatio', 'xMidYMid')
-    .attr('viewBox', '0 0 200 200')
+    .attr('viewBox', '0 0 ' + viewBox.w + ' ' + viewBox.h)
     .attr('width', '100%')
     .attr('height', '100%')
       .append('g')
-        .attr('transform', 'translate(' + w / 2 + ',' + h / 2 + ')');
+        .attr('transform', 'translate(' + viewBox.w / 2 + ',' + viewBox.h / 2 + ')');
 }
 export default chartFactory;
